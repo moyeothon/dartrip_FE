05ff9001969750b44f1ab7d17e79a34f8d4f9053
csh0616 <wetopqrss597@gmail.com>
2024-11-03 01:41:29 +0900

    feat : 팀이름, 키워드 기능 구현

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -7,9 +7,13 @@ const useStore = create((set) => ({
   setTeamName: (teamName) => set({ teamName }),
   setKeywords: (keyword) =>
     set((state) => ({
-      keywords: [...state.keywords, keyword],
+      keywords: state.keywords.includes(keyword)
+        ? state.keywords.filter((k) => k !== keyword) // 이미 선택된 경우 제거
+        : [...state.keywords, keyword],               // 선택되지 않은 경우 추가
     })),
   setPlace: (place) => set({ place }),
 }));
 
 export default useStore;
+
+
